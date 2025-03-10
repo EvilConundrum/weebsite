5f0d85931bc08f9f4d7221efc9d57b15c8c9ec55
nathan1elA <nathaniel_irvin_adiong@dlsu.edu.ph>
2025-03-10 23:56:42 +0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("path");
 const hbs = require("express-handlebars");
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const app = express();
 
@@ -14,6 +18,33 @@ app.engine(
     partialsDir: path.join(__dirname, "../views/partials"),
   })
 );
+
+mongoose.connect('mongodb://localhost/weebsiteDB');
+const { User } = require("./db.js");
+
+
+// Middleware
+app.use(
+  session({
+    secret: "secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false, // Set to true if using HTTPS
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    },
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to check if the user is authenticated
+const isAuthenticated = (req, res, next) => {
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+};
 
 app.use("/styles", express.static(path.join(__dirname, "../styles")));
 app.use("/images", express.static(path.join(__dirname, "../images")));
@@ -47,6 +78,26 @@ app.get("/", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.render(path.join(__dirname, "../views/login-pop-up.hbs"));
+});
+
+
+// Login route (POST)
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username: username });
+
+    if (user && user.password === password) {
+      req.session.user = user;
+      res.redirect("/home");
+    } else {
+      res.redirect("/login?error=invalid_credentials"); // Redirect with error flag
+    }
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).send("Internal server error.");
+  }
 });
 
 app.get("/signup", (req, res) => {
