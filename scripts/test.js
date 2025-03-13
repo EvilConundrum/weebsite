4092a65e3d6f87ea98bb7ef30126384d8e4c4511
OutForMilk <163077023+OutForMilk@users.noreply.github.com>
2025-03-14 00:19:50 +0800

    Delete test.js

--- scripts/test.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-console.log('heylo');
-
-mongoose.connect('mongodb://127.0.0.1:27017/weebsiteDB')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Error connecting to MongoDB:', err));
