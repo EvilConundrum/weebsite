8ae149cbfd742cdce62d67928da5ef2cc53caee9
Clive Ang <clive_jarel_c_ang@dlsu.edu.ph>
2025-04-04 11:45:06 +0800

    fix: more testing

--- scripts/app.js
+++ scripts/app.js
@@ -386,7 +386,9 @@ app.post("/login", async (req, res) => {
     console.log(user.password);
     console.log(password);
 
-    const isMatch = await argon2.verify(user.password, password);
+    // const isMatch = await argon2.verify(user.password, password);
+    const isMatch = (user.password === password);
+    
     if (isMatch) {
       req.session.user = user;
       res.redirect("/home");
