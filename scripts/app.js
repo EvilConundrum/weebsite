3daa3894f3a4f39df4689264b81a975ce6206218
nathan1elA <nathaniel_irvin_adiong@dlsu.edu.ph>
2025-04-04 19:41:09 +0800

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -108,7 +108,7 @@ const isAuthenticated = (req, res, next) => {
     if (req.originalUrl.startsWith("/api")) {
       return res.status(401).json({ error: "Unauthorized" });
     }
-    res.redirect("/");
+    res.redirect("/login");
   }
 };
 
